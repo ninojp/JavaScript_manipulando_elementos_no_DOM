db202da12b056f2903115586f962b310561a6469
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2023-11-24 18:16:54 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,19 @@ const elementoHTML = document.querySelector('html');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
+const elementoIMG = document.querySelector('.app__image');
 
-btnFoco.addEventListener('click', (event) => {
+btnFoco.addEventListener('click', () => {
     elementoHTML.setAttribute('data-contexto', 'foco')
-    alert(event)
+    elementoIMG.setAttribute('src', './assets/imagens/foco.png');
 });
 btnCurto.addEventListener('click', () => {
-    elementoHTML.setAttribute('data-contexto', 'descanso-curto')
+    elementoHTML.setAttribute('data-contexto', 'descanso-curto');
+    elementoIMG.setAttribute('src', './assets/imagens/descanso-curto.png');
 });
 btnLongo.addEventListener('click', (event) => {
     elementoHTML.setAttribute('data-contexto', 'descanso-longo')
+    elementoIMG.setAttribute('src', './assets/imagens/descanso-longo.png')
     // console.log(event)
-    alert(event)
+    // alert(event)
 });
